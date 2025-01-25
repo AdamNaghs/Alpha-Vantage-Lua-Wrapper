5ea84d903661356767d8b1b78416c7eebb0e62a9
Adam Naghavi <adamnaghavif@gmail.com>
2025-01-25 18:00:04 -0500

    first commit

---
+++ alpha_vantage.lua
@@ -0,0 +1,236 @@
+local M = {}
+
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+local json = require("cjson")
+
+--- Alpha Vantage docs for parameters
+M.Func = {
+    Stock =
+    {
+        -- Time Series
+        TS = {
+            Intraday = "TIME_SERIES_INTRADAY",
+            Daily = "TIME_SERIES_DAILY",
+            Daily_Adj = "TIME_SERIES_DAILY_ADJUSTED",
+            Monthly = "TIME_SERIES_MONTHLY",
+            Monthly_Adj = "TIME_SERIES_MONTHLY_ADJUSTED",
+        },
+        Quote = "GLOBAL_QUOTE",
+        Realtime_Bulk_Quotes = "REALTIME_BULK_QUOTES",
+    },
+    Options = {
+        Realtime = "REALTIME_OPTIONS",
+        Historical = "HISTORICAL_OPTIONS",
+    },
+    News = {
+        Sentiment = "NEWS_SENTIMENT",
+        Top = "TOP_GAINERS_LOSERS",
+        Insider = "INSDER_TRANSACTIONS",
+        Analytics =
+        {
+            Fixed = "ANALYTICS_FIXED_WINDOW",
+            Sliding = "ANALYTICS_SLIDING_WINDOW",
+        }
+    },
+    Data =
+    {
+        Overview = "OVERVIEW",
+        ETF_Profile = "ETF_PROFILE",
+        -- Corperate Action
+        CA = {
+            Dividends = "DIVIDENDS",
+            Splits = "SPLITS",
+        },
+        Income = "INCOME_STATEMENT",
+        Balence_Sheet = "BALANCE_SHEET",
+        Cash_Flow = "CASH_FLOW",
+        Earnings = "EARNINGS",
+        Listing_Status = "LISTING_STATUS",
+        Earnings_Calendar = "EARNINGS_CALENDAR",
+        IPO_Calendar = "IPO_CALENDAR"
+    },
+    Forex =
+    {
+        Exchange_Rate = "CURRENCY_EXCHANGE_RATE",
+        Intraday = "FX_INTRADAY",
+        Daily = "FX_DAILY",
+        Weekly = "FX_WEEKLY",
+        Monthly = "FX_MONTHLY",
+    },
+    Crypto =
+    {
+        Exchange_Rate = "CURRENCY_EXCHANGE_RATE",
+        Intraday = "CRYPTO_INTRADAY",
+        Daily = "CRYPTO_DAILY",
+        Weekly = "CRYPTO_WEEKLY",
+        Monthly = "CRYPTO_MONTHLY",
+    },
+    Commodities =
+    {
+        Crude_WTI = "WTI",
+        Crude_Brent = "BRENT",
+        Natural_Gas = "NATURAL_GAS",
+        Copper = "COPPER",
+        Aluminum = "ALUMINUM",
+        Wheat = "WHEAT",
+        Corn = "CORN",
+        Cotton = "COTTON",
+        Sugar = "SUGAR",
+        Coffee = "COFFEE",
+        All = "ALL_COMMODITIES",
+    },
+    Indicators =
+    {
+        Economic =
+        {
+            GDP = "REAL_GDP",
+            GDP_Per_Capita = "REAL_GDP_PER_CAPITA",
+            Treasury_Yield = "TREASURY_YIELD",
+            Federal_Funds_Rate = "FEDERAL_FUNDS_RATE",
+            CPI = "CPI",
+            Inflation = "INFLATION",
+            Retail_Sales = "RETAIL_SALES",
+            Durables = "DURABLES",
+            Unemployment = "UNEMPLOYMENT",
+            Nonfarm_Payroll = "NONFARM_PAYROLL",
+        },
+        Technical = {
+            -- Moving Averages
+            SMA = "SMA",     -- Simple Moving Average
+            EMA = "EMA",     -- Exponential Moving Average
+            WMA = "WMA",     -- Weighted Moving Average
+            DEMA = "DEMA",   -- Double Exponential Moving Average
+            TEMA = "TEMA",   -- Triple Exponential Moving Average
+            TRIMA = "TRIMA", -- Triangular Moving Average
+            KAMA = "KAMA",   -- Kaufman Adaptive Moving Average
+            MAMA = "MAMA",   -- MESA Adaptive Moving Average
+            T3 = "T3",       -- Triple Exponential Moving Average
+            VWAP = "VWAP",   -- Volume Weighted Average Price
+
+            -- Oscillators
+            MACD = "MACD",     -- Moving Average Convergence/Divergence
+            MACDEXT = "MACDEXT",
+            STOCH = "STOCH",   -- Stochastic Oscillator
+            STOCHF = "STOCHF", -- Stochastic Fast
+            RSI = "RSI",       -- Relative Strength Index
+            STOCHRSI = "STOCHRSI",
+            WILLR = "WILLR",   -- Williams' %R
+            ADX = "ADX",       -- Average Directional Movement Index
+            ADXR = "ADXR",     -- ADX Rating
+            APO = "APO",       -- Absolute Price Oscillator
+            PPO = "PPO",       -- Percentage Price Oscillator
+            MOM = "MOM",       -- Momentum
+            BOP = "BOP",       -- Balance of Power
+            CCI = "CCI",       -- Commodity Channel Index
+            CMO = "CMO",       -- Chande Momentum Oscillator
+            ROC = "ROC",       -- Rate of Change
+            ROCR = "ROCR",     -- Rate of Change Ratio
+            AROON = "AROON",
+            AROONOSC = "AROONOSC",
+            MFI = "MFI",       -- Money Flow Index
+            TRIX = "TRIX",     -- Triple Exponential Average
+            ULTOSC = "ULTOSC", -- Ultimate Oscillator
+            DX = "DX",         -- Directional Movement Index
+
+            -- Directional Indicators
+            MINUS_DI = "MINUS_DI",
+            PLUS_DI = "PLUS_DI",
+            MINUS_DM = "MINUS_DM",
+            PLUS_DM = "PLUS_DM",
+
+            -- Other Indicators
+            BBANDS = "BBANDS", -- Bollinger Bands
+            MIDPOINT = "MIDPOINT",
+            MIDPRICE = "MIDPRICE",
+            SAR = "SAR",       -- Parabolic SAR
+            TRANGE = "TRANGE", -- True Range
+            ATR = "ATR",       -- Average True Range
+            NATR = "NATR",     -- Normalized ATR
+            AD = "AD",         -- Chaikin A/D Line
+            ADOSC = "ADOSC",   -- Chaikin A/D Oscillator
+            OBV = "OBV",       -- On Balance Volume
+
+            -- Hilbert Transform
+            HT_TRENDLINE = "HT_TRENDLINE",
+            HT_SINE = "HT_SINE",
+            HT_TRENDMODE = "HT_TRENDMODE",
+            HT_DCPERIOD = "HT_DCPERIOD",
+            HT_DCPHASE = "HT_DCPHASE",
+            HT_PHASOR = "HT_PHASOR"
+        }
+    }
+}
+
+local function log(str)
+    local log_file = io.open("av_queries.log", "a")
+    if nil == log_file then
+        print("Could not log: ", str)
+        return
+    end
+    log_file:write(os.date("%x %X ", os.time()) .. str .. "\n")
+    log_file:close()
+end
+
+---comment
+---@param self any
+---@param func any
+---@param args any
+---@return nil|table, string|nil
+local function query(self, func, args)
+    if nil == self.api_key then
+        self.api_key = M.load_api_key()
+        if nil == self.api_key then
+            return nil, "Alpha Vantage Client missing api_key."
+        end
+    end
+    local base_url = "https://www.alphavantage.co/query?function=" .. func
+    for key, value in pairs(args or {}) do
+        base_url = base_url .. "&" .. key .. "=" .. value
+    end
+    log(base_url) -- not logging apikey
+    base_url = base_url .. "&apikey=" .. self.api_key
+    local response_body = {}
+    local _, code = http.request({
+        url = base_url,
+        method = "GET",
+        sink = ltn12.sink.table(response_body),
+        headers = { ["User-Agent"] = "Lua Alpha Vantage Client" }
+    })
+    if code ~= 200 then
+        return nil, string.format("HTTP request failed with code %d", code)
+    end
+    return json.decode(table.concat(response_body)), nil
+end
+
+---comment Expects file alpha_vantage_key.txt to exist and contain key.
+---@return string
+function M.load_api_key()
+    local file = assert(io.open("alpha_vantage_key.txt", "r"))
+    local key = file:read("*all"):gsub("^%s*(.-)%s*$", "%1")
+    file:close()
+    return key
+end
+
+function M.dump(o)
+    if type(o) ~= 'table' then
+        return tostring(o)
+    end
+    local s = '{ '
+    for k, v in pairs(o) do
+        if type(k) ~= 'number' then k = '"' .. k .. '"' end
+        s = s .. '[' .. k .. '] = ' .. M.dump(v) .. ','
+    end
+    return s .. '} '
+end
+
+
+function M.new(api_key)
+    local client = {
+        api_key = api_key,
+        query = query,
+    }
+    return client
+end
+
+return M
---
+++ poll_ticker.lua
@@ -0,0 +1,188 @@
+--- Luajit
+local av = require("alpha_vantage")
+local client = av.new(av.load_api_key())
+
+local function len(tbl)
+    local count = 0
+    for _ in pairs(tbl) do count = count + 1 end
+    return count
+end
+
+local function parse_date(date_str)
+    local year, month = date_str:match("^(%d%d%d%d)-(%d%d)")
+    if not year or not month then
+        return nil, "Invalid date format"
+    end
+    return tonumber(year), tonumber(month)
+end
+---Helper takes actual data not ticker strings
+local function calculate_beta(asset_data, benchmark_data)
+    local function get_returns(data)
+        local dates = {}
+        local price_map = {}
+
+        for date, value in pairs(data["Monthly Adjusted Time Series"]) do
+            table.insert(dates, date)
+            price_map[date] = tonumber(value["5. adjusted close"])
+        end
+
+        table.sort(dates, function(a, b) return a > b end)
+
+        local returns = {}
+        for i = 2, #dates do
+            local curr_price = price_map[dates[i]]
+            local prev_price = price_map[dates[i - 1]]
+            local return_val = (curr_price - prev_price) / prev_price
+            table.insert(returns, return_val)
+        end
+
+        return returns
+    end
+
+    local asset_returns = get_returns(asset_data)
+    local benchmark_returns = get_returns(benchmark_data)
+
+    local min_length = math.min(#asset_returns, #benchmark_returns)
+    while #asset_returns > min_length do table.remove(asset_returns) end
+    while #benchmark_returns > min_length do table.remove(benchmark_returns) end
+
+    local mean_asset = 0
+    local mean_benchmark = 0
+    for i = 1, #asset_returns do
+        mean_asset = mean_asset + asset_returns[i]
+        mean_benchmark = mean_benchmark + benchmark_returns[i]
+    end
+    mean_asset = mean_asset / #asset_returns
+    mean_benchmark = mean_benchmark / #benchmark_returns
+
+    local covariance = 0
+    local variance_benchmark = 0
+    for i = 1, #asset_returns do
+        covariance = covariance + (asset_returns[i] - mean_asset) * (benchmark_returns[i] - mean_benchmark)
+        variance_benchmark = variance_benchmark + (benchmark_returns[i] - mean_benchmark) ^ 2
+    end
+
+    covariance = covariance / (#asset_returns - 1)
+    variance_benchmark = variance_benchmark / (#asset_returns - 1)
+
+    return covariance / variance_benchmark
+end
+
+local function compute_stats(start_year, data)
+    local list = data["Monthly Adjusted Time Series"]
+    if not list then
+        return nil, "Missing time series data"
+    end
+
+    local dividend_sum = 0
+    local prices = {}
+    local dividend_months = {}
+
+    for date, value_dict in pairs(list) do
+        local year, month = parse_date(date)
+        if not year then goto continue end
+        if year < start_year then goto continue end
+
+        local div = tonumber(value_dict["7. dividend amount"])
+        local price = tonumber(value_dict["4. close"])
+
+        if div and price and div > 0 and price > 0 then
+            dividend_sum = dividend_sum + div
+            table.insert(prices, price)
+            dividend_months[year .. "-" .. month] = true
+        end
+        ::continue::
+    end
+
+    if #prices == 0 then
+        return nil, "No valid data found"
+    end
+
+    table.sort(prices)
+    local price_sum = 0
+    for _, i in ipairs(prices) do
+        price_sum = price_sum + i
+    end
+    local average_price = price_sum / #prices
+    local median_price = prices[math.ceil(#prices / 2)]
+    local annual_dividend = dividend_sum * (12 / len(dividend_months))
+    local annual_yield = (annual_dividend / median_price) * 100
+
+    return average_price, median_price, annual_dividend, annual_yield
+end
+
+-- Takes data of benchmark so we dont need to requery it and the ticker so that we can do the initial query for that asset
+local function compute_stats_against(benchmarch_data, start_year, ticker)
+    local data, error = client:query(av.Func.Stock.TS.Monthly_Adj, { symbol = ticker, outputsize = "compact" })
+    if data == nil then
+        print(string.format("Error fetching %s: %s", ticker, error))
+        return nil
+    end
+    local average_price, median_price, annual_dividend, annual_yield = compute_stats(start_year, data)
+    local beta = calculate_beta(data, benchmarch_data)
+    return average_price, median_price, annual_dividend, annual_yield, beta
+end
+
+local function create_frame(start_year, benchmark_ticker, tickers)
+    local data = {
+        headers = { "Ticker", "Avg Price", "Med Price", "Ann Div", "Yield", "Beta" },
+        rows = {}
+    }
+
+    local benchmarch = client:query(av.Func.Stock.TS.Monthly_Adj, { symbol = benchmark_ticker, outputsize = "compact" })
+    if nil == benchmarch then return nil, "Error fetching benchmark" end
+
+    for _, ticker in ipairs(tickers) do
+        local avg_price, med_price, ann_div, yield, beta = compute_stats_against(benchmarch, start_year, ticker)
+        if nil ~= avg_price then
+            table.insert(data.rows, {
+                ticker    = ticker,
+                avg_price = avg_price,
+                med_price = med_price,
+                ann_div   = ann_div,
+                yield     = yield,
+                beta      = beta
+            })
+        end
+    end
+
+    return data
+end
+
+local function print_frame(data)
+    -- Then use unpack_func instead of either unpack or table.unpack
+    print(string.format("%-6s\t%-12s\t%-12s\t%-12s\t%-12s\t%-6s",
+        ---@diagnostic disable-next-line: deprecated
+        unpack(data.headers)))
+    print(string.rep("-", 80))
+
+    -- Print rows
+    for _, row in ipairs(data.rows) do
+        print(string.format("%-6s\t$%-11.2f\t$%-11.2f\t$%-11.2f\t%-11.2f%%\t%.2f",
+            row.ticker, row.avg_price, row.med_price, row.ann_div, row.yield, row.beta))
+    end
+end
+
+local function write_csv(data, filename)
+    local file = io.open(filename, "w")
+    if not file then return nil, "Cannot open file" end
+
+    -- Write headers
+    file:write(table.concat(data.headers, ",") .. "\n")
+
+    -- Write rows
+    for _, row in ipairs(data.rows) do
+        file:write(string.format("%s,%.2f,%.2f,%.2f,%.2f,%.2f\n",
+            row.ticker, row.avg_price, row.med_price, row.ann_div, row.yield, row.beta))
+    end
+
+    file:close()
+    return true
+end
+
+local table_data = create_frame(2024, "SPY",
+    { "SPY", "PFRL", "JEPI", "JEPQ", "USFR", "TFLO", "DIVO", "JQUA", "SCHD", "PFF" })
+
+print_frame(table_data)
+
+write_csv(table_data, "etf_analysis.csv")
