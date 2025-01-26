b3e4162efb40254379de94c93b4dd2c521e6ce7a
Adam Naghavi <adamnaghavif@gmail.com>
2025-01-26 07:20:13 -0500

    New example

--- example.lua
+++ example.lua
@@ -1,7 +1,11 @@
 local market = require("market")
 
-local data = market.frame.create(2024,"SPY",{"JEPI","JEPQ","USFR","TFLO"})
+local data = market.frame.create(2024, "SPY", { "JEPI", "JEPQ", "USFR", "TFLO" })
+
+market.file.clear(market.av.log_file_path)
 
 market.frame.print(data)
 
-market.file.write_csv(data,"tmp.csv")
+market.file.write_csv(data, "tmp.csv")
+
+market.file.write_csv(market.frame.create(2024, "SPY", market.file.lines_from("all_tickers.txt")), "market.csv")
